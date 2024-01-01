a85044ca6b14d7597630c6f307a0d4c2883c304c
onfranciis <onukwuf@gmail.com>
2024-01-01 22:58:03 +0100

    feat: create NavBarLinks

---
+++ src/types/utils.ts
@@ -0,0 +1,5 @@
+export type INavBarLinks = {
+  title: string;
+  url: string;
+  expand: boolean;
+}[];
---
+++ src/utils/Links.ts
@@ -0,0 +1,29 @@
+import { INavBarLinks } from "@/types/utils";
+
+export const NavBarLinks: INavBarLinks = [
+  {
+    title: "Training",
+    url: "",
+    expand: false,
+  },
+  {
+    title: "Enterprise",
+    url: "",
+    expand: false,
+  },
+  {
+    title: "Contact",
+    url: "",
+    expand: true,
+  },
+  {
+    title: "Buy Credits",
+    url: "",
+    expand: false,
+  },
+  {
+    title: "Full Access",
+    url: "",
+    expand: true,
+  },
+];
