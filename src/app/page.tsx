fd6549d1d829fe50fac71fee7aab2b5cc02e8d3e
onfranciis <onukwuf@gmail.com>
2024-01-02 19:21:45 +0100

    feat: create and add landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
-export default function Home() {
-  return <main></main>;
-}
+import LandingPage from "@/components/landingPage/LandingPage";
+
+export default LandingPage;
---
+++ src/components/landingPage/LandingPage.tsx
@@ -0,0 +1,12 @@
+import "./LandingPage.scss";
+import One from "./One";
+
+const LandingPage = () => {
+  return (
+    <div className="LandingPage">
+      <One />
+    </div>
+  );
+};
+
+export default LandingPage;
