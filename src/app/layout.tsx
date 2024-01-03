6e59c9ef920f96ab435f64922b7b3cd403e41b93
onfranciis <onukwuf@gmail.com>
2024-01-03 16:15:15 +0100

    feat: set og image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,35 @@ import "../styles/globals.scss";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import FirstChild from "./FirstChild";
+import OG from "../../public/opengraph.png";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "OSINT Industries",
-  description: "",
+  description: "Landing page demo by Franics Onukwu",
+  icons: {
+    icon: "/icon.png",
+  },
+  metadataBase: new URL("https://sustainobles.com.ng/"),
+  openGraph: {
+    images: [
+      {
+        url: OG.src,
+        width: OG.width,
+        height: OG.height,
+      },
+    ],
+  },
+  twitter: {
+    images: [
+      {
+        url: OG.src,
+        width: OG.width,
+        height: OG.height,
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
